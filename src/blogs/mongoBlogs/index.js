90969351c0e85698f17e973e0f9cfcd1722eeb5d
Kajal lathiya <kajalbodarya77@gmail.com>
2023-01-17 20:55:11 +0530

    add crud mongo apis

---
+++ src/blogs/mongoBlogs/index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import StiveBlogsModel from "./model.js"; // mongodb collection model
+import createHttpError from "http-errors";
+
+const mongoBlogsRouter = express.Router();
+
+mongoBlogsRouter.post("/", async (req, res, next) => {
+  try {
+    const newBlogPost = new StiveBlogsModel(req.body);
+    const { _id } = await newBlogPost.save();
+    res.status(201).send({ id: _id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+mongoBlogsRouter.get("/", async (req, res, next) => {
+  try {
+    const blogPosts = await StiveBlogsModel.find();
+    res.status(200).send(blogPosts);
+  } catch (error) {
+    next(error);
+  }
+});
+
+mongoBlogsRouter.get("/:postId", async (req, res, next) => {
+  try {
+    const blogPost = await StiveBlogsModel.findById(req.params.postId);
+    if (blogPost) res.send(blogPost);
+    else
+      next(
+        createHttpError(404, `User with id ${req.params.postId} not found!`)
+      );
+  } catch (error) {
+    next(error);
+  }
+});
+
+mongoBlogsRouter.put("/:postId", async (req, res, next) => {
+  try {
+    const updatedPost = await StiveBlogsModel.findByIdAndUpdate(
+      req.params.postId,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (updatedPost) res.send(updatedPost);
+    else
+      next(
+        createHttpError(404, `User with id ${req.params.postId} not found!`)
+      );
+  } catch (error) {
+    next(error);
+  }
+});
+
+mongoBlogsRouter.delete("/:postId", async (req, res, next) => {
+  try {
+    const deletedPost = await StiveBlogsModel.findByIdAndDelete(
+      req.params.postId
+    );
+    if (deletedPost) res.status(204).send();
+    else
+      next(
+        createHttpError(404, `User with id ${req.params.postId} not found!`)
+      );
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default mongoBlogsRouter;
--- src/server.js
+++ src/server.js
@@ -1,36 +1,56 @@
 import express from "express";
 import cors from "cors";
-import { join } from "path"
+import { join } from "path";
 import listEndpoints from "express-list-endpoints";
 import blogPostsRouter from "./blogs/index.js";
 import filesRouter from "./blogs/files/index.js";
+
+// mongo collection
 import mongoose from "mongoose";
+import mongoBlogsRouter from "./blogs/mongoBlogs/index.js";
+import createHttpError from "http-errors";
 
 const server = express();
 const port = process.env.PORT;
- 
-const publicFolderPath = join(process.cwd(), "./public")
 
+const publicFolderPath = join(process.cwd(), "./public");
+//**************************** MIDDLEWARE *********************************** */
 const loggerMiddleware = (req, res, next) => {
   console.log(`Request method ${req.method} url ${req.url}`);
-  next()
+  next();
 };
 
-server.use(express.static(publicFolderPath))
-server.use(cors());
-server.use(loggerMiddleware)
-server.use(express.json());
-server.use("/blogPosts",loggerMiddleware, blogPostsRouter);
-server.use("/files", loggerMiddleware, filesRouter);
+server.use(express.static(publicFolderPath));
 
-mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGO_URL)
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_DEV_URL];
+const corsOptions = {
+  origin: (origin, corsNext) => {
+    console.log("CURRENT ORIGIN: ", origin);
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      corsNext(null, true);
+    } else {
+      createHttpError(400, `Origin ${origin} is not in whitelist`);
+    }
+  }
+};
+server.use(cors(corsOptions));
+
+server.use(loggerMiddleware);
+server.use(express.json());
+
+//**************************** ENDPOINTS *********************************** */
+
+server.use("/blogPosts", loggerMiddleware, blogPostsRouter);
+server.use("/files", loggerMiddleware, filesRouter);
+server.use("/mongoBlogPosts", mongoBlogsRouter);
+
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGO_URL);
 
 mongoose.connection.on("connected", () => {
-  console.log('Successfully connected to Mongo!');
+  console.log("Successfully connected to Mongo!");
   server.listen(port, () => {
     console.table(listEndpoints(server));
     console.log(`server is running on port: ${port}`);
   });
-})
-
+});
